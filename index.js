561d97ee3bf8a81eb4c5b0fd0ad8a841e2b6f518
G Arun Kumar <garunk0703@gmail.com>
2015-04-26 00:24:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+	response.render("chat");
+});
+
+var io = require('socket.io').listen(app.listen(app.get('port')));
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    
+    socket.on('locdata', function (data) {
+    	console.log("Location data: " + data);
+    	io.sockets.emit('locdata-client', data);
+    	io.sockets.emit('message', {message: data});
+    });
+
+    socket.on('streamurl', function (data) {
+    	console.log("Stream URL: " + data);
+    });
+
+    socket.on('send', function (data) {
+    	console.log(data);
+        io.sockets.emit('message', data);
+    });
+});
+
+console.log("Listening on port " + app.get('port'));
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'));
+// });
---
+++ public/chat.js
@@ -0,0 +1,34 @@
+var socket;
+var field;
+var sendButton;
+var content;
+var messages;
+
+window.onload = function() {
+ 
+    messages = [];
+    socket = io.connect('https://lit-inlet-4843.herokuapp.com');
+    field = $(".field");
+    sendButton = $(".send");
+    content = $("#content");
+ 
+    socket.on('message', function (data) {
+        console.log(data);
+        if(data.message) {
+            messages.push(data.message);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += messages[i] + '<br />';
+            }
+            $(content).html(html);
+            //content.innerHTML = html;
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+ 
+    $(sendButton).click(function() {
+        var text = $(field).val();
+        socket.emit('send', {'message': text});
+    });
+}
