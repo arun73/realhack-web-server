ca32c8aae2b807fb64fbe24cb7490608e0fb9360
G Arun Kumar <garunk0703@gmail.com>
2015-04-26 09:13:55 +0530

    minor commit

---
+++ public/js/socket-client.js
@@ -0,0 +1,30 @@
+var socket;
+
+window.onload = function() {
+ 
+    messages = [];
+    socket = io.connect('http://realhack-server.herokuapp.com');
+    // field = $(".field");
+    // sendButton = $(".send");
+    // content = $("#content");
+ 
+    // socket.on('message', function (data) {
+    //     console.log(data);
+    //     if(data.message) {
+    //         messages.push(data.message);
+    //         var html = '';
+    //         for(var i=0; i<messages.length; i++) {
+    //             html += messages[i] + '<br />';
+    //         }
+    //         $(content).html(html);
+    //         //content.innerHTML = html;
+    //     } else {
+    //         console.log("There is a problem:", data);
+    //     }
+    // });
+ 
+    // $(sendButton).click(function() {
+    //     var text = $(field).val();
+    //     socket.emit('send', {'message': text});
+    // });
+}
