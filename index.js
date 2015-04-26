58b43b3194da6e6985d072896eeb612f41803cb1
G Arun Kumar <garunk0703@gmail.com>
2015-04-26 09:01:59 +0530

    minor commit

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
-app.set('view engine', "html");
+// app.set('view engine', "html");
 //app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
 	// response.render("index.");
-    response.send('hi');
+    response.render('index.html');
 });
 
 var io = require('socket.io').listen(app.listen(app.get('port')));
